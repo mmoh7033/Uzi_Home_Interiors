a13c350743cab7f8f73298dfbd7b7c91277c9ca2
mmoh7033 <106455332+mmoh7033@users.noreply.github.com>
2025-02-25 08:06:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2821
+\cocoatextscaling1\cocoaplatform1{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red198\green120\blue221;\red171\green178\blue191;\red152\green195\blue121;
+}
+{\*\expandedcolortbl;;\cssrgb\c77647\c47059\c86667;\cssrgb\c67059\c69804\c74902;\cssrgb\c59608\c76471\c47451;
+}
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs28 \cf2 function\cf3  showAlert() \{\
+    alert(\cf4 "Thank you for your interest! We'll contact you soon."\cf3 );\
+\}}
